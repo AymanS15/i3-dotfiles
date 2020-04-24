bb4105fb04aea229b9a6d31236847c39c6537e69
Ayman S15 <48180533+AymanS15@users.noreply.github.com>
2020-04-24 16:12:31 +0000

    Add files via upload

---
+++ _.config/i3/current_brightness.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# base dir for backlight class
+basedir="/sys/class/backlight/"
+
+# get the backlight handler
+handler=$basedir$(ls $basedir)"/"
+
+# get current brightness
+old_brightness=$(cat $handler"brightness")
+
+echo $old_brightness
